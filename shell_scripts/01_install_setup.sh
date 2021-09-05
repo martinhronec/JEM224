555265b416cd66b2cb2dd5bb24ae299ac4981602
Martin Hronec <hroneq@gmail.com>
2021-09-05 23:40:22 +0200

    Clear repo before the lectures and update README.

--- shell_scripts/01_install_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git config --global user.name "<your name>"
-git config --global user.email "<your email>"
-# git config --global core.editor "code --wait" (for VS code)
-git config --global core.editor "vim"
-
-git config --list
